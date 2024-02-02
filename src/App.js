df91362303058332ec89a2a7733ccbd3667522f1
Edward Ang <wei.hen.ang@gmail.com>
2024-02-02 15:56:39 +0800

    login_done_commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import "./App.css"
 import Toast from 'react-bootstrap/Toast';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Home from "./pages/home"
 
-function App() {
+const Login = () => {
+  let navigate = useNavigate();
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
@@ -32,7 +35,7 @@ function App() {
       // If login is successful, navigate to the home page
       if (data.message === 'Login successful') {
         // Redirect to the home page
-        return <Redirect to="/home" />;
+        navigate('/home');
       }
     } catch (error) {
       console.error('Error:', error);
@@ -57,15 +60,15 @@ function App() {
 
   return (
     <div className="App">
-      <div class="login-container">
-        <div class="login-header">
+      <div className="login-container">
+        <div className="login-header">
           <div>
             Login
           </div>
         </div>
-        <input type="text" class="login-input" placeholder="Username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-        <input type="password" class="login-input" placeholder="Password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <button class="login-button" id="login-button" onClick={handleLogin}>Login</button>
+        <input type="text" className="login-input" placeholder="Username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" className="login-input" placeholder="Password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button className="login-button" id="login-button" onClick={handleLogin}>Login</button>
       </div>
       <Toast id='toast' show={showA} onClose={toggleShowA} delay={3000} autohide>
         <Toast.Body id='toastBody'><i className="bi bi-info-circle"></i>{message && <div>{message}</div>}</Toast.Body>
@@ -74,4 +77,12 @@ function App() {
   );
 }
 
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  </Router>
+);
 export default App;
