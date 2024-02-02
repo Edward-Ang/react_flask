dc997f6e0cbb44b495ff1a859631301dd6a1848c
Edward Ang <wei.hen.ang@gmail.com>
2024-02-02 15:03:20 +0800

    second_commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
+import "./App.css"
+import Toast from 'react-bootstrap/Toast';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import Home from "./pages/home"
 
 function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
+  const [showA, setShowA] = useState(false);
+
+  const toggleShowA = () => setShowA(!showA);
 
   const handleLogin = async () => {
     try {
@@ -18,8 +26,14 @@ function App() {
       const data = await response.json();
 
       setMessage(data.message);
+      setShowA(!showA);
 
       console.log(data.message);
+      // If login is successful, navigate to the home page
+      if (data.message === 'Login successful') {
+        // Redirect to the home page
+        return <Redirect to="/home" />;
+      }
     } catch (error) {
       console.error('Error:', error);
     }
@@ -43,22 +57,19 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Login Form</h1>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <br />
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={handleRegister}>Register</button>
-      <div>
-        {message && <div>{message}</div>}
+      <div class="login-container">
+        <div class="login-header">
+          <div>
+            Login
+          </div>
+        </div>
+        <input type="text" class="login-input" placeholder="Username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" class="login-input" placeholder="Password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button class="login-button" id="login-button" onClick={handleLogin}>Login</button>
       </div>
+      <Toast id='toast' show={showA} onClose={toggleShowA} delay={3000} autohide>
+        <Toast.Body id='toastBody'><i className="bi bi-info-circle"></i>{message && <div>{message}</div>}</Toast.Body>
+      </Toast>
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    {/* Your home page component */}
+    <h1>Welcome to the Home Page!</h1>
+  </div>
+);
+
+export default Home;
